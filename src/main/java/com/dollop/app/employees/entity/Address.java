63233a84954d34f66dee1d573e850918396d68fe
OM <OM@DESKTOP-P4UKRHU>
2026-06-04 23:56:23 +0530

    I have developed permission role and user entity

---
+++ src/main/java/com/dollop/app/employees/entity/Address.java
@@ -0,0 +1,40 @@
+package com.dollop.app.employees.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "addresses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID addressId;
+
+    private String addressLine1;
+
+    private String addressLine2;
+
+    private String city;
+
+    private String state;
+
+    private String country;
+
+    private String zipCode;
+}
